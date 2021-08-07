c93ef6cf65405bfb2928d4e605b0476ed2c37269
Jose Luis <joseluis15_1997@hotmail.com>
2021-08-06 20:45:15 -0500

    segundo commit

--- css/validador.js
+++ css/validador.js
@@ -6,8 +6,8 @@ function validar_ingreso() {
     usuario = document.getElementById("username").value;
     contrasena = document.getElementById("password").value;
     if (usuarios.includes(usuario) && contrasena === "estrellitas123") {
-        window.open("https://www.argar.cat");
-        //window.open("https://www.argar.cat","_self");
+        //window.open("https://www.argar.cat");
+        window.open("https://www.argar.cat","_self");
     } else {
         alertify.error('Usuario o contraseña incorrectos');
     }
